1f8313faf4040bd2a771f8d65ea46a87266e0150
AimForNaN <aimfornan@localhost>
2026-07-21 06:06:54 -0400

    Updated tests.

--- tests/NaN/Collections/CollectionTest.php
+++ tests/NaN/Collections/CollectionTest.php
@@ -3,12 +3,53 @@
 use NaN\Collections\Collection;
 
 describe('Collection', function () {
-	test('Count', function () {
+	test('Array conversion', function () {
+		$collection = new Collection();
+
+		expect($collection->toArray())
+			->toBeEmpty()
+		;
+
 		$collection = new Collection(1,2,3);
+
+		expect($collection->toArray())
+			->toBe([1,2,3])
+		;
+	});
+
+	test('Count', function () {
+		$collection = new Collection();
+
+		expect($collection)->toBeEmpty();
+
+		$collection = new Collection(1,2,3);
+
 		expect($collection)->toHaveCount(3);
+
+		$collection = new class() extends Collection {
+			public function __construct(...$items) {
+				parent::__construct(...$items);
+			}
+		};
+
+		expect($collection)
+			->toBeEmpty()
+			->and($collection->toArray())
+				->toBeEmpty()
+		;
 	});
 
 	test('Map', function () {
+		$collection = new Collection();
+		$mappings = $collection->map(fn($item) => (string)$item);
+		$mappings = [...$mappings];
+
+		expect($mappings)
+			->toBeEmpty()
+			->and($mappings)
+				->toBe([])
+		;
+
 		$collection = new Collection(1,2,3);
 		$mappings = $collection->map(fn($item) => (string)$item);
 		$mappings = [...$mappings];
@@ -22,6 +63,7 @@ describe('Collection', function () {
 
 	test('Splat operator', function () {
 		$collection = new Collection(1,2,3);
+
 		expect([...$collection])->toBe([1,2,3]);
 	});
 });
